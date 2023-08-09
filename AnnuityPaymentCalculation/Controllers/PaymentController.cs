bbf8410d308e65b76f3c4d2e93d377d629282887
Gg <Garn87@list.ru>
2023-08-09 20:45:52 +0500

    добавил вью

--- AnnuityPaymentCalculation/Controllers/PaymentController.cs
+++ AnnuityPaymentCalculation/Controllers/PaymentController.cs
@@ -23,6 +23,11 @@ namespace AnnuityPaymentCalculation.Controllers
             return View();
         }
 
+        public IActionResult Test()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
